525def46d2071df3b09562871db93fc39607cbdb
bdombry <bdombry>
2025-02-11 09:00:51 +0100

    clean imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { PokemonService } from './core/services/pokemon.service';
 import { UtilsService } from './core/services/utils.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
