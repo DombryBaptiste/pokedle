2718f11d4c6783c07e41bd37b6765474f823e592
bdombry <bdombry>
2025-02-20 14:11:13 +0100

    Ajout des slide toggle de générations

--- src/app/features/game/game.component.ts
+++ src/app/features/game/game.component.ts
@@ -4,21 +4,31 @@ import { PokemonService } from '../../core/services/pokemon.service';
 import { Pokemon } from '../../models/pokemon';
 import { map, Observable, startWith } from 'rxjs';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { AsyncPipe, JsonPipe } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { PokemonDetails } from '../../models/pokemon-detail';
 import { PokemonDetailComponent } from '../pokemon-detail/pokemon-detail.component';
+import { MatSlideToggleChange, MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 
 
 @Component({
   selector: 'app-game',
-  imports: [MatAutocompleteModule, ReactiveFormsModule, AsyncPipe, MatInputModule, MatIconModule, MatButtonModule, PokemonDetailComponent, JsonPipe],
+  imports: [MatAutocompleteModule, ReactiveFormsModule, AsyncPipe, MatInputModule, MatIconModule, MatButtonModule, PokemonDetailComponent, MatSlideToggleModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
 export class GameComponent implements OnInit {
+
+  listGeneration: {name: string, range: [number, number]}[] = [
+    { name: "Gen 1", range: [1, 151]},
+    { name: "Gen 2", range: [152, 251]},
+    { name: "Gen 3", range: [252, 386]},
+    { name: "Gen 4", range: [387, 493]},
+    { name: "Gen 5", range: [494, 649]}
+  ]
 
   listPokemon: Pokemon[] = [];
   filteredListPokemon: Observable<Pokemon[]> | undefined;
@@ -53,6 +63,11 @@ export class GameComponent implements OnInit {
       }
     });
     this.listPokemon = this.listPokemon.filter(pokemon => pokemon.id !== pokemonSelected.id);
+  }
+
+  toggleChange(gen:any, event: MatSlideToggleChange) {
+    console.log(event);
+    console.log(gen)
   }
 
   private filterPokemons(value: string): Pokemon[] {
